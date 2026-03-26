92a1fdca760b89c394e7f8e5d78b1b19dd890413
Simran Rawat <rawatsimran822@gmail.com>
2026-03-26 10:46:16 +0000

    if else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+// //if
+// const isUserLoggedIn=true;
+// if(isUserLoggedIn){
+//     console.log("loggedinnode");
+// }
+// //<,>,<=,>=,==,!=,===
+// console.log(true == 1);    // true 
+// console.log(true === 1);   // false
+
+// console.log(null == undefined);   // true
+// console.log(null === undefined);  // false
+
+// console.log(0 == false);   // true
+// console.log(0 === false);  // false
+//== type conversion bhi kr deta h  and comaprison bhi but === strict equal;ity hai yhe value and type dono check krta h and no type conversion 
+
+const score=200
+if(score>100){
+    const power="fly"
+    console.log(`User Power: ${power}`)
+}
+ //console.log(`User Power: ${power}`)//power is not defined 
+
+const balance=1000
+// if(balance>500)
+//     console.log("test"),
+// console.log("test2");//comma lga ke aage bhi scope ho skta hai but it is not a good prctice 
+
+//if,else if,else similar in cpp
+//&& ,||
