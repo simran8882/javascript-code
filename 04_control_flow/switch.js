0a34aca4e9058916707c1803b8d25de41b7161b7
Simran Rawat <rawatsimran822@gmail.com>
2026-03-26 10:44:28 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month=3
+switch(month){
+    case 1:
+        console.log("January");
+            break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;//break nhi lgaya toh saara aage ka code bhi execute ho jayega except default
+    default:
+        console.log("default case");
+        break; 
+     
+
+        
+}
